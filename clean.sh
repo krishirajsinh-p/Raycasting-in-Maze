4f269a09be803d91dbe4cad686d27c417a309c24
krishirajsinh-p <krishirajpuwar@gmail.com>
2026-02-21 12:50:31 +0100

    Add build and clean scripts, update package list, and remove to-do file

---
+++ clean.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+BLUE="\033[1;34m"
+YELLOW="\033[1;33m"
+NC="\033[0m"
+
+print_error() {
+    echo -e "${RED}✖ $1${NC}"
+}
+
+print_success() {
+    echo -e "${GREEN}✔ $1${NC}"
+}
+
+print_msg() {
+    echo -e "${BLUE}➜ $1${NC}"
+}
+
+print_warning() {
+    echo -e "${YELLOW}⚠ $1${NC}"
+}
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+BUILD_DIR="$PROJECT_ROOT/build"
+PACKAGE_FILE="$PROJECT_ROOT/packages.txt"
+
+print_msg "Starting the cleaning process..."
+print_msg "removing build directory and installed packages..."
+
+echo ""
+
+if [ -d "$BUILD_DIR" ]; then
+    print_msg "Cleaning build directory $BUILD_DIR ..."
+    rm -rf "$BUILD_DIR"
+    if [ $? -eq 0 ]; then
+        print_success "Build directory cleaned successfully."
+    else
+        print_error "Failed to clean build directory. Do it manually."
+    fi
+else
+    print_warning "No build directory found at $BUILD_DIR, Nothing to clean."
+fi
+
+echo ""
+
+PACKAGES=()
+while IFS= read -r package; do
+    if ! dpkg -s "$package" &> /dev/null; then
+        print_warning "Package $package is not installed. Skipping."
+    else
+        PACKAGES+=("$package")
+        print_msg "Package $package will be removed."
+    fi
+done < "$PACKAGE_FILE"
+
+if [ ${#PACKAGES[@]} -ne 0 ]; then
+    print_msg "Removing packages..."
+    sudo apt remove -y "${PACKAGES[@]}" -qq &> /dev/null
+    if [ $? -eq 0 ]; then
+        print_success "Packages removed successfully."
+    else
+        print_error "Failed to remove some packages. Do it manually."
+    fi
+else
+    print_warning "No packages to remove."
+fi
+
+echo ""
+
+print_success "Cleaning process completed."
--- run.sh
+++ run.sh
@@ -1,6 +1,86 @@
-cat packages.txt | xargs sudo apt install -y
-mkdir build
-cd build
-cmake ..
-make
-./Raycasting-in-Maze
+#!/bin/bash
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+BLUE="\033[1;34m"
+YELLOW="\033[1;33m"
+NC="\033[0m"
+
+print_error() {
+    echo -e "${RED}✖ $1${NC}"
+}
+
+print_success() {
+    echo -e "${GREEN}✔ $1${NC}"
+}
+
+print_msg() {
+    echo -e "${BLUE}➜ $1${NC}"
+}
+
+print_warning() {
+    echo -e "${YELLOW}⚠ $1${NC}"
+}
+
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+
+PACKAGE_FILE="$PROJECT_ROOT/packages.txt"
+BUILD_DIR="$PROJECT_ROOT/build"
+EXECUTABLE="$BUILD_DIR/Raycasting-in-Maze"
+
+print_msg "Starting build process..."
+echo ""
+
+if [ ! -f "$PACKAGE_FILE" ]; then
+    print_error "Package file $PACKAGE_FILE not found!"
+    exit 1
+fi
+
+print_msg "Checking required packages..."
+
+MISSING_PACKAGES=()
+while IFS= read -r package; do
+    if ! dpkg -s "$package" &> /dev/null; then
+        print_warning "Package $package is not installed."
+        MISSING_PACKAGES+=("$package")
+    else
+        print_success "Package $package is already installed. Skipping."
+    fi
+done < "$PACKAGE_FILE"
+
+if [ ${#MISSING_PACKAGES[@]} -ne 0 ]; then
+    print_msg "Installing missing packages..."
+    sudo apt update -qq &> /dev/null
+    sudo apt install -y "${MISSING_PACKAGES[@]}" -qq &> /dev/null
+    print_success "Dependencies installed."
+else
+    print_success "All dependencies are installed."
+fi
+
+echo ""
+
+if [ -d "$BUILD_DIR" ]; then
+    print_msg "build found. Rebuilding now..."
+else
+    print_msg "No build found. Building now..."
+    mkdir -p "$BUILD_DIR"
+fi
+
+cd "$BUILD_DIR"
+
+cmake -DCMAKE_BUILD_TYPE=Release .. &> /dev/null
+make -j$(nproc) &> /dev/null
+
+cd "$PROJECT_ROOT"
+
+if [ ! -f "$EXECUTABLE" ]; then
+    print_error "Build failed."
+    exit 1
+else
+    print_success "Build succeeded."
+fi
+
+echo ""
+
+print_success "Running $EXECUTABLE..."
+"$EXECUTABLE"
